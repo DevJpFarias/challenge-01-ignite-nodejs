8ff806655de70baf67eab7c3e6ba875e5627bde8
João <joaopfc2764@gmail.com>
2023-08-01 18:16:56 -0300

    refactor: add tasks routes on app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { tasksRoutes } from './routes/tasks'
 
 export const app = fastify()
+
+app.register(tasksRoutes, {
+  prefix: 'tasks'
+})
 
 app.get('/tables', async () => {
   const tables = await knex('sqlite_schema').select('*')
