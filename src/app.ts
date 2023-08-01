75a7aad05da32fb44040390958ab92d7f633c885
João <joaopfc2764@gmail.com>
2023-08-01 15:03:08 -0300

    feat: create test route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 export const app = fastify()
+
+app.get('/tables', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
